215e8fbe5146ac399e37cdd6d4925924a74cf62e
geekhall <174821300@qq.com>
2022-01-29 22:48:14 +0800

    modify readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
+// import { resolve} from 'path'
 import vue from '@vitejs/plugin-vue'
 
+// function pathResolver(dir: string) {
+//   return resolve(__dirname, ".", dir);
+// }
+// module.export = {}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
