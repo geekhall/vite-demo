137963411950851a9432797baba480b2aa407e72
geekhall <174821300@qq.com>
2022-02-02 12:17:57 +0800

    20220202-121757

--- src/view/Test.vue
+++ src/view/Test.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="test_container">
     <h1>用于测试Vue3和Vue2的区别</h1>
-    <h2>测试插值语法, {{name}}</h2>
+    <h2 class="title">插值语法</h2>
+    <h3> Hello , {{name}}</h3>
     <a v-bind:href="url">Home</a>
-
-    <h1>单向数据绑定</h1>
+    <hr/><br/>
+    <h2 class="title">单向数据绑定</h2>
     <input type="text" v-bind:value="name1"> <br/>
     <input type="text" :value="name1">
-    <hr/>
-    <h1>双向数据绑定</h1>
+    <br/><br/>
+    <h2 class="title">双向数据绑定</h2>
 <!--    下面的写法在Vue3中会报错：-->
 <!--    <input type="text" v-model:value="name2">-->
     <input type="text" v-model="name2"><br/>
     <input type="text" v-model="name2">
-    <hr>
+    <hr/><br/>
+    <h2 class="title">Methods</h2>
     <h2>{{count}}</h2>
     <button @click="increment">点我++</button><br/>
     <!-- 简写形式 -->
@@ -23,13 +25,19 @@
     <!-- 传递参数, not working -->
 <!--    <button @click="showInfo3($event,666)">点我弹出提示信息3</button><br/>-->
 
-    <hr/>
-    <h2>使用计算属性实现姓名合并</h2>
+    <hr/><br/>
+    <h2 class="title">计算属性</h2>
     姓：<input type="text" v-model="firstName"/><br/><br/>
     名：<input type="text" v-model="lastName"/><br/><br/>
     <!-- 全名： <span>{{fullName()}}</span> -->
     全名： <span>{{computedFullName}}</span><br/>
     全名1： <span>{{computedFullName1}}</span><br/>
+    <hr/><br/>
+    <h2 class="title">监视属性</h2>
+    <h2>今天天气很{{weatherInfo}}</h2>
+    <button @click="changeWeather">切换天气</button>
+    <hr/>
+
   </div>
 </template>
 
@@ -48,7 +56,8 @@ export default defineComponent({
       name1: "name1",
       name2: "name2",
       firstName: "",
-      lastName: ""
+      lastName: "",
+      isHot: true
     }
   },
   methods: {
@@ -65,6 +74,9 @@ export default defineComponent({
       console.log(number);    // 点我弹出提示信息
       alert(number);
     },
+    changeWeather() {
+      this.isHot = !this.isHot
+    }
     // showInfo3(number: PointerEvent){
     //   console.log(number);    // 点我弹出提示信息
     //   alert(number);
@@ -81,11 +93,25 @@ export default defineComponent({
       get(){
         return this.firstName + ' ' + this.lastName;
       }
+    },
+    weatherInfo: function (){
+      return this.isHot ? '炎热' : '清爽';
+    }
+  },
+  watch: { // 监视属性
+    isHot: {
+      immediate: true, // 初始化时让handler调用一下
+      // handler函数在isHot发生改变的时候调用。
+      handler(newValue, oldValue){
+        console.log('isHot被修改了', newValue, oldValue);
+      }
     }
   }
 })
 </script>
 
 <style lang="less" scoped>
-
+.title {
+  color: cadetblue;
+}
 </style>
