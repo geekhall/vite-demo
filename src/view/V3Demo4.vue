4b02b8ecf8541a56e1e2371f21533c22b615cce1
geekhall <174821300@qq.com>
2022-02-04 09:33:51 +0800

    20220204-093351

--- src/view/V3Demo4.vue
+++ src/view/V3Demo4.vue
@@ -1,29 +1,22 @@
 <template>
     <h1>Son.</h1>
-    {{msg}}
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
     import { reactive, computed} from 'vue'
-
-    export default {
-        props: {
-            msg: String
-        },
-        // setup(props, ctx) {
-        //     console.log("setup -> props: ", props);
-        //     const state = reactive({
-        //         num1: 0,
-        //         num2: 0,
-        //         result: computed(() => parseInt(state.num1) + parseInt(state.num2));
-        //     })
-        //     const clickEvent = () => {
-        //         console.log("button clicked! ");
-        //         ctx.emit('sendMsg', state.result)
-        //     }
-        //     return {};
-        // },
-    }
+    // setup(props, ctx) {
+    //     console.log("setup -> props: ", props);
+    //     const state = reactive({
+    //         num1: 0,
+    //         num2: 0,
+    //         result: computed(() => parseInt(state.num1) + parseInt(state.num2));
+    //     })
+    //     const clickEvent = () => {
+    //         console.log("button clicked! ");
+    //         ctx.emit('sendMsg', state.result)
+    //     }
+    //     return {};
+    // },
 </script>
 
 <style lang="less" scoped>
